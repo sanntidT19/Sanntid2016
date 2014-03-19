66e4c7118521eb9c605aa98f3fbaf8bbea78cf30
sanntidT19 <yngveska@stud.ntnu.no>
2014-03-19 05:59:30 +0100

    Early morning show
    Algorythm is working!

--- src/optimal/OTest.go
+++ src/optimal/OTest.go
@@ -1,4 +1,4 @@
-package optimal
+package main
 
 import ( 
 	"fmt"
@@ -25,29 +25,30 @@ type Cabin struct {
 	HasIntOrders bool
 }
 
+func main() {
+// H are orders from hallway
+var H [2][FLOORS] bool
+//Y is mirror of hallway
+var Y [2][FLOORS] bool
 // C is array of orders from cabin
 var C[ELEV] Cabin
-// H are orders from hallway
-
-// Yet to serve - mirror of hallway, but will clear until every order from hallway is issued - I guess we should'n delete values in H
-var H [2][FLOORS] bool
-var Y [2][FLOORS] bool
 var Outotal [2][FLOORS] bool
-var TimeArr[ELEV] int
+var E [ELEV][2][FLOORS] bool
+var T[ELEV] int
 
-func main() {
+
 	// clears the command line
 	cmd := exec.Command("cmd", "/c", "cls")
     cmd.Stdout = os.Stdout
     cmd.Run()
 	// starting conditions of elevators
-	dire := []bool{true, false, true}
+	dire := []bool{true, true, true}
 	posit := []int {3,2,2}
 	
 	// Input from inside cabins
-	InBut := []bool{false, true, false, false,
+	InBut := []bool{false, false, false, false,
 			false, false, false, false,
-			false, false, false, true,}
+			false, false, false, false,}
 	
 	//input from hallway - top and bottom floor can only go one direction
 	OuUp := [FLOORS]bool{true, true, false, false}
@@ -59,29 +60,62 @@ func main() {
 
 	//input from hallway into struct	
 	H = Outotal
+	// make mirror of hallway - we will not overwrite hallway
 	Y = H
 
-	// For each elevator do:
+	fmt.Println("External list",H)
+	//filling of cabins
 	for i := 0; i < ELEV; i++ {
 		
 		C[i] = Cabin{i, posit[i], dire[i], InBut[i*FLOORS:i*FLOORS+FLOORS],0,0,0,0,false} // fill recieved data to C array
+
+
+		//Check if elevator needs to change direction - it happens if there is no order in original direction
+		C[i].Direction = DirectionSwitch(C[i].Direction, C[i].HighestFloor, C[i].LowestFloor, C[i].Position)
+
+		E[i] = Transform(C[i].Buttons[0:FLOORS], C[i].Direction, C[i].Position)
+
+		// Gets rid of duplicitous floors allready served by cabins
+		Y = (Allready(H, C[i].Buttons[0:FLOORS], C[i].Direction, C[i].Position))
+		 
+	}
+
+
+	// giving orders to elevator with current least load
+	for j := 0; j < FLOORS; j++ {
+		for n := 0; n < 2; n++ {
+				if Y[n][j]{
+					E, T = loop(C[0:ELEV], Y, E)
+					E[Leastloadfu(T[0:ELEV])][n][j] = true
+					
+					}
+				}
+			}	
+
+					fmt.Println("Toto je E0",E[0])
+					fmt.Println("Toto je E1",E[1])
+					fmt.Println("Toto je E2",E[2])
+}
+
+
+ func loop(C[] Cabin, H[2][FLOORS] bool,  ExecutionList[ELEV][2][FLOORS] bool) ([ELEV][2][FLOORS]bool, [ELEV]int){
+ 	var TimeArr[ELEV] int
+	// For each elevator do:
+	for i := 0; i < ELEV; i++ {
 		
+
 		// Find Highest floor, Lowest floor, Number of stops, and if it has any orders.
-		C[i].HighestFloor, C[i].LowestFloor, C[i].Stops, C[i].HasIntOrders = FindHighLow(C[i].Buttons[0:FLOORS])
+		C[i].HighestFloor, C[i].LowestFloor, C[i].Stops, C[i].HasIntOrders = FindHighLow(ExecutionList[i])
 
-		//Check if elevator needs to change direction
-    	C[i].Direction = DirectionSwitch(C[i].Direction, C[i].HighestFloor, C[i].LowestFloor, C[i].Position)
+		    	
     	C[i].Timeload = Counttimeload(C[i].HasIntOrders, C[i].Direction, C[i].HighestFloor, C[i].LowestFloor, C[i].Position, C[i].Stops)	
-    	fmt.Println("External list",H)
-		H = (Allready(H, C[i].Buttons[0:FLOORS], C[i].Direction, C[i].Position))
-		fmt.Println("External list",H)		
-		// get rid of floors inside of elevator movements
-		//
-		//
 
-	TimeArr[i] = C[i].Timeload
+    			
+		// creates array of timeloads for 
+		TimeArr[i] = C[i].Timeload
 
-
+	
+	
 	// Just for testing
 	fmt.Println("Name = ",C[i].Name)
 	fmt.Println("Position",i, " = ",C[i].Position)
@@ -93,10 +127,10 @@ func main() {
 	fmt.Println("Timeload",i, " = ", C[i].Timeload)
 	fmt.Println()
 	}
-	// finds the number of elevator with least load
-	Leastload := Leastloadfu(TimeArr[0:ELEV])	
-	fmt.Println("Number of Leastload",Leastload)
-	// = InsideMovement(C[Leastload] Cabin)
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	return ExecutionList, TimeArr
 }
 
 //selects the elevator with least load	
@@ -117,12 +151,10 @@ func DirectionSwitch(Di bool, Hi int, Lo int, Po int) bool{
 		V := Di
 		if Di && Hi <= Po{
 			V = false
-			fmt.Println("switch down")
 		}
 		
 		if !Di && Lo >= Po{
 			V = true
-			fmt.Println("switch UP")
 		}
 		
 	return V
@@ -162,15 +194,16 @@ func Allready(P[2][FLOORS] bool, Bu[] bool, Di bool, Po int) [2][FLOORS]bool{
 		return P	
 }
 // Finds Highest and Lowest number, Counts number of stops and says if elevator has internal orders at all
-func FindHighLow(Bu[] bool) (int, int, int, bool){
+func FindHighLow(Ex[2][FLOORS] bool) (int, int, int, bool){
 		counter := false // counts if lowest level was allready found
 		Ha := false
 		St := 0
 		Hi := 0
 		Lo := 0
+
 		for j := 0; j < FLOORS; j++ {
 			// find highest odered floor
-        	if Bu[j] {
+        	if Ex[0][j] ||  Ex[1][j]{
 				Hi = j
 
 				// find lowest odered floor
@@ -184,12 +217,42 @@ func FindHighLow(Bu[] bool) (int, int, int, bool){
 		
 			}
 			// false if there are no inside orders
-			if Bu[j] {
+			if Ex[0][j] || Ex[1][j] {
 				Ha = true
 			}
     	}
     return Hi, Lo, St, Ha
 }
+// Transforms cabin buttons into execution list
+func Transform(Bu[] bool, Di bool, Po int) [2][FLOORS]bool{
+			var P[2][FLOORS] bool
+			for j := 0; j < FLOORS; j++ {
+				if Bu[j]{
+					// Solves if elevator should handle floor on the way up or down.
+					switch{
+					 	case Po < j :
+							P[0][j] = true
+						case Po > j :
+							P[1][j] = true
+						case Po == j :
+							// if button is pressed when cabin is in the same floor, it is considered as elevator already left
+							if Di {
+								P[1][j] = true
+							}else {
+								P[0][j] = true
+							}
+					}
+				}
+			}
+		return P	
+}
+/*
+func HallwayToExec(P[][][] bool,) [2][FLOORS]bool{
+		
+		return P	
+}
+*/
+
 /*
 func InsideMovement(P[]) {
 	
