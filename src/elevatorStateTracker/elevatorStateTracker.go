89f54f87f30214ba071d7ae1355f1508aa532e5b
yngveska <yngve.skaalerud@gmail.com>
2016-02-08 16:37:32 +0100

    Added functions for reading and writing the state to file. Not tested yet.

--- src/elevatorStateTracker/elevatorStateTracker.go
+++ src/elevatorStateTracker/elevatorStateTracker.go
@@ -1,9 +1,13 @@
 import(
 	"../globalStructs"
+	"encoding/gob"
+	"os"
+	"fmt"
 )
 
 currentState ElevatorState
 
+const PATH_OF_SAVED_STATE = "elevState.gob"
 
 func initalize_state_tracker(){
 	//read from file to check if system was killed
@@ -14,16 +18,57 @@ func initalize_state_tracker(){
 }
 
 func send_updated_elevator_state(){
-	//call this whenever its updated 
+	//call this whenever its updated. write to channels
 }
 
 func write_elevator_state_to_file(){
+	//temp for testing
+	test_struct := ElevatorState{255,2,1,1,0}
 	//update this whenever the local elevator gets an order/command
+	dataFile, err := os.Create(PATH_OF_SAVED_STATE)
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	dataEncoder := gob.NewEncoder(dataFile)
+	dataEncoder.Encode(test_struct)
+	dataFile.Close()
 }
 
 func read_elevator_state_from_file(){
+	//start with reading it
+	var data ElevatorState
+
+	if _, err := os.Stat("PATH_OF_SAVED_STATE"); os.IsNotExist(err){
+		fmt.Println("Local save of elevator state not detected. It has been cleared/this is the first run on current PC")
+		return
+	}
+	dataFile, err := os.Open(PATH_OF_SAVED_STATE)
+
+	dataDecoder := gob.NewDecoder(dataFile)
+	err = dataDecoder.Decode(&data)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	dataFile.Close()
+
+	currentState = data
 }
 
-func detect_system_killed(){
-	//maybe a different name
+func detect_system_killed() bool{
+	//only call when initializing, REDO WHEN WE KNOW HOW THE ORDERS WILL LOOK
+	for i:= 0; i < NUM_FLOORS; i++{
+		for j:= 0; j < NUM_BUTTONS; j++{
+			/*if currentState.orders[i][j] != 0{
+				return true
+			}
+			*/
+			if currentState.orders == 1{
+				return true
+			}
+		}
+	}
+	return false
 }
