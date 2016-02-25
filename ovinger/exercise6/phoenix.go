e90dce0fbfed85a54e8e4c55270526deca43c69c
Student <student@itk-d650>
2016-02-25 12:23:33 +0100

    exercise6 more progress

--- ovinger/exercise6/phoenix.go
+++ ovinger/exercise6/phoenix.go
@@ -5,7 +5,8 @@ import (
 	"io/ioutil"
 	"net"
 	"os"
-	"os/exec"
+	//"os/exec"
+	"encoding/binary"
 	"time"
 )
 
@@ -19,84 +20,127 @@ simple counter that writes to file.
 Simple reader that reads from file when it is master
 */
 func main() {
-	addr := net.UDPAddr{
+	/*addr := net.UDPAddr{
 		Port: 20058,
 		IP:   net.ParseIP("129.241.187.151"),
-	}
-	conn, _ := net.ListenUDP("129.241.187.151", &addr)
+	}*/
+
 	fmt.Println("I am backup")
+
+	localIP := "129.241.187.151"
+	localPort := "20058"
+	localAddr := localIP + ":" + localPort
+
+	udpAddr, err := net.ResolveUDPAddr("udp4", localAddr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	connection, err := net.ListenUDP("udp4", udpAddr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Listening on port ", localPort)
+	defer connection.Close()
+
 	masterChan := make(chan bool)
-	go detect_precense(conn, masterChan)
+	go detect_precense(connection, masterChan)
 
 	<-masterChan
 	fmt.Println("I am master")
-	spawn_backup()
-	counterChan := make(chan int)
+	//spawn_backup()
+	counterChan := make(chan int64)
 	go read_from_file(counterChan)
-	go spam_precense()
+	go spam_precense(localAddr)
 	go counter_and_write_to_file(counterChan)
 	time.Sleep(time.Second * 10000)
 }
 
-func counter_and_write_to_file(counterChan chan int) {
+func counter_and_write_to_file(counterChan chan int64) {
 	counter := <-counterChan
+	var binary_counter []byte
 	for {
 		counter++
-		ioutil.WriteFile(FILENAME, counter, os.ModeExclusive)
-		fmt.Println(counter)
+		fmt.Println("counter1", counter)
+		_ = binary.PutVarint(binary_counter, counter)
+		ioutil.WriteFile(FILENAME, binary_counter, os.ModeExclusive)
+		fmt.Println("counter2", counter)
 		time.Sleep(time.Millisecond * 200)
 	}
 }
 
-func read_from_file(counterChan chan int) {
-	fromFile, err := ioutil.ReadFile(FILENAME)
-	if err != nil {
-		panic(err)
+//Need to check for the case when there's nothing there.
+func read_from_file(counterChan chan int64) {
+	var counter int64
+	if _, err := os.Stat("FILENAME"); os.IsNotExist(err) {
+		fmt.Println("no save exists")
+		counter = 0
+	} else {
+		fromFile, err := ioutil.ReadFile(FILENAME)
+		if err != nil {
+			fmt.Println(err)
+		}
+		counter, _ = binary.Varint(fromFile)
 	}
-	counter := int(fromFile)
 	counterChan <- counter
 }
 
+/*
 func spawn_backup() {
 	cmd := exec.Command(gnome-terminal-x[""], "go run phoenix.go")
 	err := exec.Run(cmd)
 	if err != nil {
-		fmt.Println("You fucked up in spawn_backup")
+		fmt.Println("You messed up in spawn_backup")
 		panic(err)
 	}
 }
+*/
+func spam_precense(localAddr string) {
+	udpRemote, _ := net.ResolveUDPAddr("udp", localAddr)
 
-func spam_precense() {
-	conn, err := net.Dial("udp", LOCAL_ADDR)
+	connection, err := net.DialUDP("udp", nil, udpRemote)
 	if err != nil {
+		fmt.Println("You messed up in spam presenceclear")
 		panic(err)
 	}
+	defer connection.Close()
 	for {
-		_, err := conn.WriteToUDP([]byte("I am the master motherfucker"))
+		_, err := connection.Write([]byte("I am the master"))
 		if err != nil {
-			fmt.Println("You fucked up in spam_precense")
+			fmt.Println("You messed up in spam_precense")
 			panic(err)
 		}
 		time.Sleep(time.Millisecond * 500)
 	}
 }
 
-func detect_precense(conn *UDPConn, masterChan chan bool) {
-	timerChan := time.After(time.Second * 3)
-	p := make([]byte, 2048)
-L:
+func detect_precense(connection *net.UDPConn, masterChan chan bool) {
+	//timerChan := time.After(time.Second * 3)
+	buffer := make([]byte, 2048)
 	for {
-		select {
-		case <-timerChan:
-			fmt.Println("Master dead")
+		t := time.Now()
+		connection.SetDeadline(t.Add(3 * time.Second))
+		_, _, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("UDP timeout: ", err)
 			masterChan <- true
-			break L
-		default:
-			_, _, err := conn.ReadFromUDP(p)
-			if err != nil {
-				fmt.Println("You fucked up in detect_precense.")
-				panic(err)
-			}
+			break
 		}
+		/*
+			fmt.Println("I'm getting into this for")
+			select {
+			case <-time.After(time.Second * 3):
+				fmt.Println("Master dead")
+				masterChan <- true
+				break L
+				/*
+					default:
+						_, _, err := connection.ReadFromUDP(buffer)
+						if err != nil {
+							fmt.Println("You messed up in detect_precense.")
+							panic(err)
+						}
+			}(*/
 	}
 }
