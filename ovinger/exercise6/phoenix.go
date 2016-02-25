9715a0eb1b918ff11f6c3d2b6cb32f2d02c57061
Student <student@itk-d650>
2016-02-25 15:33:58 +0100

    exercise 6 finished, panic may cause the program to fuck up. Handle otherwise in future

--- ovinger/exercise6/phoenix.go
+++ ovinger/exercise6/phoenix.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	//"encoding/binary"
+	"encoding/gob"
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"net"
 	"os"
-	//"os/exec"
-	"encoding/binary"
+	"os/exec"
+	"strconv"
 	"time"
 )
 
-const FILENAME = "counterSave.txt"
+const FILENAME = "counterSave.gob"
+const PORT_HAX_FILE = "port.gob"
 
 /*
 Need:
@@ -28,9 +31,12 @@ func main() {
 	fmt.Println("I am backup")
 
 	localIP := "129.241.187.151"
-	localPort := "20058"
+	localPort := read_port_from_file()
+	sendToPort := write_next_port_to_file(localPort)
 	localAddr := localIP + ":" + localPort
+	sendToAddr := localIP + ":" + sendToPort
 
+	fmt.Println("this is sendToAddr: ", sendToAddr)
 	udpAddr, err := net.ResolveUDPAddr("udp4", localAddr)
 
 	if err != nil {
@@ -49,59 +55,86 @@ func main() {
 
 	<-masterChan
 	fmt.Println("I am master")
-	//spawn_backup()
-	counterChan := make(chan int64)
+	spawn_backup()
+	counterChan := make(chan int)
 	go read_from_file(counterChan)
-	go spam_precense(localAddr)
+	time.Sleep(time.Second)
+	go spam_precense(sendToAddr)
 	go counter_and_write_to_file(counterChan)
 	time.Sleep(time.Second * 10000)
 }
 
-func counter_and_write_to_file(counterChan chan int64) {
+func counter_and_write_to_file(counterChan chan int) {
 	counter := <-counterChan
-	var binary_counter []byte
+	//binary_counter := make([]byte, 2048)
 	for {
 		counter++
-		fmt.Println("counter1", counter)
-		_ = binary.PutVarint(binary_counter, counter)
-		ioutil.WriteFile(FILENAME, binary_counter, os.ModeExclusive)
-		fmt.Println("counter2", counter)
+
+		dataFile, err := os.Create(FILENAME)
+
+		if err != nil {
+			fmt.Println("Error while writing next port to file")
+			panic(err)
+		}
+
+		dataEncoder := gob.NewEncoder(dataFile)
+		dataEncoder.Encode(counter)
+		dataFile.Close()
+		fmt.Println(counter)
 		time.Sleep(time.Millisecond * 200)
 	}
 }
 
 //Need to check for the case when there's nothing there.
-func read_from_file(counterChan chan int64) {
-	var counter int64
-	if _, err := os.Stat("FILENAME"); os.IsNotExist(err) {
-		fmt.Println("no save exists")
+func read_from_file(counterChan chan int) {
+
+	var data int
+	var counter int
+
+	if _, err := os.Stat(FILENAME); os.IsNotExist(err) {
 		counter = 0
 	} else {
-		fromFile, err := ioutil.ReadFile(FILENAME)
+		dataFile, err := os.Open(FILENAME)
+		dataDecoder := gob.NewDecoder(dataFile)
+		err = dataDecoder.Decode(&data)
+
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("error in reading counter")
+			panic(err)
 		}
-		counter, _ = binary.Varint(fromFile)
-	}
+		counter = data
+	} /*
+		if _, err := os.Stat("FILENAME"); os.IsNotExist(err) {
+			fmt.Println("no save exists")
+			counter = 0
+		} else {
+			fromFile, err := ioutil.ReadFile(FILENAME)
+			if err != nil {
+				fmt.Println(err)
+			}
+			counter, _ = binary.Varint(fromFile)
+		}*/
 	counterChan <- counter
 }
 
-/*
+//This in command-window:  gnome-terminal -x sh -c 'go run phoenix.go;sh'
+
 func spawn_backup() {
-	cmd := exec.Command(gnome-terminal-x[""], "go run phoenix.go")
-	err := exec.Run(cmd)
+	command := exec.Command("gnome-terminal", "-x", "sh", "-c", "go run phoenix.go")
+	err := command.Run()
 	if err != nil {
 		fmt.Println("You messed up in spawn_backup")
 		panic(err)
 	}
+	fmt.Println("backup should be spawned")
 }
-*/
-func spam_precense(localAddr string) {
-	udpRemote, _ := net.ResolveUDPAddr("udp", localAddr)
+
+func spam_precense(remoteAddr string) {
+	udpRemote, _ := net.ResolveUDPAddr("udp", remoteAddr)
 
 	connection, err := net.DialUDP("udp", nil, udpRemote)
 	if err != nil {
-		fmt.Println("You messed up in spam presenceclear")
+		fmt.Println("You messed up in spam presence")
 		panic(err)
 	}
 	defer connection.Close()
@@ -116,11 +149,10 @@ func spam_precense(localAddr string) {
 }
 
 func detect_precense(connection *net.UDPConn, masterChan chan bool) {
-	//timerChan := time.After(time.Second * 3)
 	buffer := make([]byte, 2048)
 	for {
 		t := time.Now()
-		connection.SetDeadline(t.Add(3 * time.Second))
+		connection.SetReadDeadline(t.Add(3 * time.Second))
 		_, _, err := connection.ReadFromUDP(buffer)
 		if err != nil {
 			fmt.Println("UDP timeout: ", err)
@@ -143,4 +175,39 @@ func detect_precense(connection *net.UDPConn, masterChan chan bool) {
 						}
 			}(*/
 	}
+}
+
+func write_next_port_to_file(portNum string) string {
+	portNumToFile, _ := strconv.Atoi(portNum)
+	portNumToFile++
+	dataFile, err := os.Create(PORT_HAX_FILE)
+
+	if err != nil {
+		fmt.Println("Error while writing next port to file")
+		panic(err)
+	}
+
+	dataEncoder := gob.NewEncoder(dataFile)
+	dataEncoder.Encode(portNumToFile)
+	dataFile.Close()
+
+	return strconv.Itoa(portNumToFile)
+}
+
+func read_port_from_file() string {
+	var data int
+
+	if _, err := os.Stat(PORT_HAX_FILE); os.IsNotExist(err) {
+		return "20058"
+	}
+	dataFile, err := os.Open(PORT_HAX_FILE)
+	dataDecoder := gob.NewDecoder(dataFile)
+	err = dataDecoder.Decode(&data)
+
+	if err != nil {
+		fmt.Println("error in reading port")
+		panic(err)
+	}
+	portToRead := strconv.Itoa(data)
+	return portToRead
 }
