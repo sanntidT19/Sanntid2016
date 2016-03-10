901d5c876b7b816f9cb22932dd513285a2746243
Duvarahan Kirishnathasan <duva_92@msn.com>
2016-03-10 16:26:07 +0100

    added basic network code, to be tested next time

--- src/communication/communication.go
+++ src/communication/communication.go
@@ -1,22 +1,39 @@
-package communication //main for når denne skal kjøres, utelukkende
+package main //communication for når denne skal kjøres, utelukkende
 
 import (
+	"bytes"
 	"fmt"
 	"net"
+	"reflect"
 	"time"
-	//"reflect"
-	"bytes"
 )
 
-/*
+var broadcastAddr string = "129.241.187.255:20059"
+var commonPort string = "20059"
+
 func main() {
-	fmt.Println("Main started")
-	localIP := getLocalIP()
-	fmt.Println(localIP)
-	time.Sleep(time.Second * 1)
+	localAddr := getLocalIP()
+	if localAddr == "" {
+		fmt.Println("Problem using getLocalIP")
+	}
+	fmt.Println(localAddr)
+	spam_precense(broadcastAddr)
+	connection := establish_connection(localAddr, commonPort)
+	go readMessages(connection)
+	defer connection.Close()
+	time.Sleep(time.Second * 1000)
 }
-*/
-func getLocalIP() net.IP {
+
+func establish_connection(remoteIp string, remotePort string) *net.UDPConn {
+	fullAddr := remoteIp + ":" + remotePort
+	remoteUDPAddr, _ := net.ResolveUDPAddr("udp4", fullAddr)
+	connection, _ := net.ListenUDP("udp4", remoteUDPAddr)
+
+	return connection
+
+}
+
+func getLocalIP() string {
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		fmt.Println("Whoops..")
@@ -35,11 +52,41 @@ func getLocalIP() net.IP {
 			case *net.IPAddr:
 				ip = v.IP
 			}
-			//fmt.Println("Printing type: ", reflect.TypeOf(ip))
+			fmt.Println("Printing type: ", reflect.TypeOf(ip))
 			if bytes.Compare(ip, ipLow) >= 0 && bytes.Compare(ip, ipHigh) <= 0 {
-				return ip
+				return ip.String()
 			}
 		}
 	}
-	return nil
+	return ""
+}
+
+func spam_precense(remoteAddr string) {
+	udpRemote, _ := net.ResolveUDPAddr("udp", remoteAddr)
+
+	connection, err := net.DialUDP("udp", nil, udpRemote)
+	if err != nil {
+		fmt.Println("You messed up in spam presence")
+		panic(err)
+	}
+	defer connection.Close()
+	for {
+		_, err := connection.Write([]byte("I am here"))
+		if err != nil {
+			fmt.Println("You messed up in spam_precense")
+			panic(err)
+		}
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+func readMessages(remoteUDPConn *net.UDPConn) {
+	buffer := make([]byte, 2048)
+	for {
+		_, senderAddr, err := remoteUDPConn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Error reading message, do nothing")
+		}
+		fmt.Println("Sender: ", senderAddr.IP.String())
+	}
 }
