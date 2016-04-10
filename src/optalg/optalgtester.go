58443cd93ca6254fd05fefeef2330f6439cf43a5
Pedon Lindberg <yngveska@stud.ntnu.no>
2016-04-10 14:51:15 +0200

    Update optalgtester.go
    Changed all names to make them more descriptive of their contents

--- src/optalg/optalgtester.go
+++ src/optalg/optalgtester.go
@@ -25,64 +25,65 @@ var el_state3 ElevatorState = ElevatorState{MyIP: "123.123.123.125",
 	Direction:    1,
 	OrderQueue:       []Button{Button{2, -1, false}}}
 */
-var allElevs []ElevatorState
+var allElevStates []ElevatorState
 
-func Opt_alg(new_order Order) string {
+func OptAlg(newOrder Order) string {
 	numOfElevs := len(allElevs)
-	all_elevs := make([]ElevatorState, numOfElevs)
-	copy(all_elevs, allElevs)
+	elevStatesCopy := make([]ElevatorState, numOfElevs)
+	copy(elevStatesCopy, allElevStates)
 
-	IP_cost_list := make([]int, numOfElevs)
-	Queue_len_list := make([]int, numOfElevs)
-	var Ele_nmr int = -1
-	var IP_score int = 100
-	var Optimal_IP string = "0"
+	IPCostList := make([]int, numOfElevs)
+	queueLenList := make([]int, numOfElevs)
+	var elevNmr int = -1
+	var lowestCost int = 100
+	var optimalIP string = "0"
 	fmt.Println("optalg: number of elevs seen: ", numOfElevs)
-	for i, v := range all_elevs {
-		Queue_len_list[i] = len(v.OrderQueue)
-		if v.CurrentFloor < new_order.Floor {
+	for i, v := range allOptElevs {
+		queueLenList[i] = len(v.OrderQueue)
+		if v.CurrentFloor < newOrder.Floor {
 			if v.Direction == DOWN {
-				IP_cost_list[i] += 1
+				IPCostList[i] += 1
 			}
 		} else if v.CurrentFloor > new_order.Floor {
 			if v.Direction == UP {
-				IP_cost_list[i] += 1
+				IPCostList[i] += 1
 				//Add distance to last order in queue and distance from last order to new order, only if there are orders
 				if len(v.OrderQueue) > 0{
 					lastFloorInOrderQueue := v.OrderQueue[len(v.OrderQueue) -1].Floor
 					
-					floorsToBeVisited := int(math.Abs(float64(v.CurrentFloor - lastFloorInOrderQueue)) + math.Abs(float64(new_order.Floor - lastFloorInOrderQueue)))
+					floorsToBeVisited := int(math.Abs(float64(v.CurrentFloor - lastFloorInOrderQueue)) + math.Abs(float64(newOrder.Floor - lastFloorInOrderQueue)))
 
 
-					IP_cost_list[i] += floorsToBeVisited
+					IPCostList[i] += floorsToBeVisited
 				}
 			}
 		}
-		float_difference := float64(v.CurrentFloor - new_order.Floor)
-		IP_cost_list[i] += int(math.Abs(float_difference))
-		IP_cost_list[i] += len(v.OrderQueue)
+		floatDifference := float64(v.CurrentFloor - newOrder.Floor)
+		
+		IPCostList[i] += int(math.Abs(floatDifference))
+		IPCostList[i] += len(v.OrderQueue)
 	}
-	for k := 0; k < len(IP_cost_list); k += 1 {
-		if IP_cost_list[k] < IP_score {
-			Optimal_IP = all_elevs[k].IP
-			IP_score = IP_cost_list[k]
-			Ele_nmr = k
-		} else if IP_cost_list[k] == IP_score {
-			if len(all_elevs[k].OrderQueue) < len(all_elevs[Ele_nmr].OrderQueue) {
-				Optimal_IP = all_elevs[k].IP
-				Ele_nmr = k
-			} else if len(all_elevs[k].OrderQueue) == len(all_elevs[Ele_nmr].OrderQueue) {
-				if all_elevs[k].IP > Optimal_IP {
-					Optimal_IP = all_elevs[k].IP
-					Ele_nmr = k
+	for k := 0; k < len(ipCostList); k += 1 {
+		if IPCostList[k] < lowestCost {
+			optimalIP = elevStatesCopy[k].IP
+			lowestCost = IPCostList[k]
+			elevNmr = k
+		} else if IPCostList[k] == lowestCost {
+			if len(elevStatesCopy[k].OrderQueue) < len(elevStatesCopy[ElevNmr].OrderQueue) {
+				optimalIP = elevStatesCopy[k].IP
+				ElevNmr = k
+			} else if len(elevStatesCopy[k].OrderQueue) == len(elevStatesCopy[Ele_nmr].OrderQueue) {
+				if elevStatesCopy[k].IP > OptimalIP {
+					optimalIP = elevStatesCopy[k].IP
+					ElevNmr = k
 				}
 			}
 		}
 	}
 	fmt.Println("For this order: ")
-	stateMachine.PrintOrder(new_order)
-	fmt.Println("My choice: ", Optimal_IP)
-	return Optimal_IP
+	stateMachine.PrintOrder(newOrder)
+	fmt.Println("My choice: ", optimalIP)
+	return optimalIP
 }
 
 /*
@@ -91,7 +92,7 @@ func main() {
 }
 */
 func GetOrderQueueOfDeadElev(deadIP string) []Order {
-	for _, v := range allElevs {
+	for _, v := range allElevStates {
 		if v.IP == deadIP {
 			listCopy := make([]Order, len(v.OrderQueue))
 			copy(listCopy, v.OrderQueue)
@@ -109,26 +110,26 @@ func UpdateElevatorStateList() {
 		case updatedElevState := <-FromNetworkNewElevStateChan:
 			fmt.Println("New state received!")
 			elevInList := false
-			for i, v := range allElevs {
+			for i, v := range allElevStates {
 				if updatedElevState.IP == v.IP {
-					allElevs[i] = updatedElevState
+					allElevStates[i] = updatedElevState
 					elevInList = true
 					break
 				}
 			}
 			if !elevInList {
-				allElevs = append(allElevs, updatedElevState)
+				allElevStates = append(allElevStates, updatedElevState)
 			}
 		case elevatorTakesOrder := <-AddOrderAssignedToElevStateChan:
-			for _, v := range allElevs {
+			for _, v := range allElevStates {
 				if elevatorTakesOrder.AssignedTo == v.IP {
 					v.OrderQueue = append(v.OrderQueue, elevatorTakesOrder.Order)
 				}
 			}
 		case deadElev := <-ToOptAlgDeleteElevChan:
-			for i, v := range allElevs {
+			for i, v := range allElevStates {
 				if v.IP == deadElev {
-					allElevs = append(allElevs[:i], allElevs[i+1:]...)
+					allElevStates = append(allElevStates[:i], allElevStates[i+1:]...)
 					break
 				}
 			}
